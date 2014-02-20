9effef25d3c1112e35586ea04611dd63df22d855
Dror Cohen <drorcohe@gmail.com>
2014-02-20 10:30:47 +0200

    first commit

---
+++ src/com/drorcohe/helloworldproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.drorcohe.helloworldproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		TextView tv = (TextView)findViewById(R.id.str1);
+		tv.setText("Hello World");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
